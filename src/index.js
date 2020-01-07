926959b6393dee378e4d6f2ccf0c5edaf4a380e9
selinahsu <selinaraindrop@gmail.com>
2020-01-07 18:05:46 -0500

    First commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import ToDo from "./ToDo";
+  
+ReactDOM.render(
+    <div>
+        <ToDo />
+    </div>,
+    document.getElementById("root")
+);
